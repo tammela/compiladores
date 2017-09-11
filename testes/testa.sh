8849b260ba8f4f37b705b020a8cbf4400feeb57c
Pedro Tammela <pctammela@getcujo.com>
2017-09-11 09:22:25 -0300

    bison grammar

--- testes/testa.sh
+++ testes/testa.sh
@@ -2,11 +2,11 @@ for f in testes/*.m;
 do
    echo "testando $f"
    tmp=$(mktemp testes/tmp.XXX)
-   $1 $f > $tmp
-   if diff -q $tmp $f.gab ; then
+   $1 $f > $tmp 2>/dev/null
+   if diff -Z -q $tmp $f.gab ; then
       echo "OK";
    else
       echo "NOT OK";
    fi
-   rm $tmp
+#   rm $tmp
 done
